10b87dce1fd585850ddc6be02b55b381df6dcd00
manydots <manydots@163.com>
2019-10-12 10:09:22 +0800

    :wrench: fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,37 @@
 var config = require('./webpack.base.config');
+var path = require('path');
 var fs = require('fs-extra');
 
 module.exports = (env, argv) => {
-    //console.log(argv.mode)
+    //console.log(config.output.path)
     if (argv.mode === 'production') {
         //var CleanWebpackPlugin = require('clean-webpack-plugin');
         //config.plugins.push(new CleanWebpackPlugin())
     } else if (argv.mode === 'development') {
-        var path = require('path');
         var buildPath = path.resolve(__dirname, 'build/lib');
-        if (!fs.existsSync(buildPath)) {
+        if (!fs.existsSync(buildPath) || findSync(buildPath).length <= 0) {
             require('./src/utils/index').build();
         }
     };
     return config;
+};
+
+function findSync(startPath) {
+    let result = [];
+    function finder(paths) {
+        let files = fs.readdirSync(paths);
+        files.forEach((val, index) => {
+            let fPath = path.join(paths, val);
+            let stats = fs.statSync(fPath);
+            if (stats.isDirectory()) {
+                finder(fPath)
+            };
+            if (stats.isFile()) {
+                result.push(fPath)
+            };
+        });
+
+    };
+    finder(startPath);
+    return result;
 }
